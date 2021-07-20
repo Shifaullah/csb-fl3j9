335676f64df9b71e45daa3ab7378bba208ab5922
Code Sandbox <hello@codesandbox.io>
2021-07-20 20:40:10 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.b3ba1c30.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{4:function(e,c,t){"use strict";t.r(c);t(0);var r,n=t(3),s=t.n(n),i=(t(9),t(2));s.a.render(Object(i.jsx)("div",{children:Object(i.jsx)("h1",{children:"shifa"})}),(r="#root",document.querySelector(r)))},9:function(e,c,t){}},[[4,1,2]]]);
+//# sourceMappingURL=main.b3ba1c30.chunk.js.map
